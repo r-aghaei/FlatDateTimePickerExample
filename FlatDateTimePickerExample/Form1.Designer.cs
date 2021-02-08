baebdcde66f7f3321d57145d4cc6117d2e0124f8
Reza Aghaei <r.aghaei@outlook.com>
2021-02-08 19:58:30 +0800

    Add project files.

---
+++ FlatDateTimePickerExample/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿
+namespace FlatDateTimePickerExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flatDateTimePicker6 = new FlatDateTimePicker();
+            this.flatDateTimePicker5 = new FlatDateTimePicker();
+            this.flatDateTimePicker15 = new FlatDateTimePicker();
+            this.flatDateTimePicker4 = new FlatDateTimePicker();
+            this.flatDateTimePicker2 = new FlatDateTimePicker();
+            this.flatDateTimePicker3 = new FlatDateTimePicker();
+            this.flatDateTimePicker9 = new FlatDateTimePicker();
+            this.flatDateTimePicker1 = new FlatDateTimePicker();
+            this.SuspendLayout();
+            // 
+            // flatDateTimePicker6
+            // 
+            this.flatDateTimePicker6.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.flatDateTimePicker6.Enabled = false;
+            this.flatDateTimePicker6.Location = new System.Drawing.Point(10, 75);
+            this.flatDateTimePicker6.Name = "flatDateTimePicker6";
+            this.flatDateTimePicker6.Size = new System.Drawing.Size(225, 25);
+            this.flatDateTimePicker6.TabIndex = 4;
+            // 
+            // flatDateTimePicker5
+            // 
+            this.flatDateTimePicker5.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.flatDateTimePicker5.Enabled = false;
+            this.flatDateTimePicker5.Location = new System.Drawing.Point(10, 106);
+            this.flatDateTimePicker5.Name = "flatDateTimePicker5";
+            this.flatDateTimePicker5.ShowCheckBox = true;
+            this.flatDateTimePicker5.Size = new System.Drawing.Size(225, 25);
+            this.flatDateTimePicker5.TabIndex = 5;
+            // 
+            // flatDateTimePicker15
+            // 
+            this.flatDateTimePicker15.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.flatDateTimePicker15.Location = new System.Drawing.Point(10, 13);
+            this.flatDateTimePicker15.Name = "flatDateTimePicker15";
+            this.flatDateTimePicker15.Size = new System.Drawing.Size(225, 25);
+            this.flatDateTimePicker15.TabIndex = 6;
+            // 
+            // flatDateTimePicker4
+            // 
+            this.flatDateTimePicker4.BorderColor = System.Drawing.Color.Gold;
+            this.flatDateTimePicker4.Enabled = false;
+            this.flatDateTimePicker4.Location = new System.Drawing.Point(241, 106);
+            this.flatDateTimePicker4.Name = "flatDateTimePicker4";
+            this.flatDateTimePicker4.ShowCheckBox = true;
+            this.flatDateTimePicker4.ShowUpDown = true;
+            this.flatDateTimePicker4.Size = new System.Drawing.Size(225, 25);
+            this.flatDateTimePicker4.TabIndex = 7;
+            // 
+            // flatDateTimePicker2
+            // 
+            this.flatDateTimePicker2.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.flatDateTimePicker2.Location = new System.Drawing.Point(10, 44);
+            this.flatDateTimePicker2.Name = "flatDateTimePicker2";
+            this.flatDateTimePicker2.ShowCheckBox = true;
+            this.flatDateTimePicker2.Size = new System.Drawing.Size(225, 25);
+            this.flatDateTimePicker2.TabIndex = 8;
+            // 
+            // flatDateTimePicker3
+            // 
+            this.flatDateTimePicker3.BorderColor = System.Drawing.Color.DeepSkyBlue;
+            this.flatDateTimePicker3.Enabled = false;
+            this.flatDateTimePicker3.Location = new System.Drawing.Point(241, 75);
+            this.flatDateTimePicker3.Name = "flatDateTimePicker3";
+            this.flatDateTimePicker3.ShowUpDown = true;
+            this.flatDateTimePicker3.Size = new System.Drawing.Size(225, 25);
+            this.flatDateTimePicker3.TabIndex = 9;
+            // 
+            // flatDateTimePicker9
+            // 
+            this.flatDateTimePicker9.BorderColor = System.Drawing.Color.Gold;
+            this.flatDateTimePicker9.Location = new System.Drawing.Point(241, 44);
+            this.flatDateTimePicker9.Name = "flatDateTimePicker9";
+            this.flatDateTimePicker9.ShowCheckBox = true;
+            this.flatDateTimePicker9.ShowUpDown = true;
+            this.flatDateTimePicker9.Size = new System.Drawing.Size(225, 25);
+            this.flatDateTimePicker9.TabIndex = 10;
+            // 
+            // flatDateTimePicker1
+            // 
+            this.flatDateTimePicker1.BorderColor = System.Drawing.Color.DeepSkyBlue;
+            this.flatDateTimePicker1.Location = new System.Drawing.Point(241, 13);
+            this.flatDateTimePicker1.Name = "flatDateTimePicker1";
+            this.flatDateTimePicker1.ShowUpDown = true;
+            this.flatDateTimePicker1.Size = new System.Drawing.Size(225, 25);
+            this.flatDateTimePicker1.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(476, 144);
+            this.Controls.Add(this.flatDateTimePicker6);
+            this.Controls.Add(this.flatDateTimePicker5);
+            this.Controls.Add(this.flatDateTimePicker15);
+            this.Controls.Add(this.flatDateTimePicker4);
+            this.Controls.Add(this.flatDateTimePicker2);
+            this.Controls.Add(this.flatDateTimePicker3);
+            this.Controls.Add(this.flatDateTimePicker9);
+            this.Controls.Add(this.flatDateTimePicker1);
+            this.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private FlatDateTimePicker flatDateTimePicker6;
+        private FlatDateTimePicker flatDateTimePicker5;
+        private FlatDateTimePicker flatDateTimePicker15;
+        private FlatDateTimePicker flatDateTimePicker4;
+        private FlatDateTimePicker flatDateTimePicker2;
+        private FlatDateTimePicker flatDateTimePicker3;
+        private FlatDateTimePicker flatDateTimePicker9;
+        private FlatDateTimePicker flatDateTimePicker1;
+    }
+}
+
